a48076c99b446e2b917b4b682b515117ee1e4ae3
hungtq1016 <hungtq1016@gmail.com>
2023-01-06 01:14:26 +0700

    update plural

--- app/Http/Controllers/DbController.php
+++ app/Http/Controllers/DbController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class DbController extends Controller
 {
     public function index(Request $request)
     {
         // DB::table($request->value);
-        $columns = Schema::getColumnListing($request->value); // users table
+        $plural = Str::plural($request->value);
+        $columns = Schema::getColumnListing($plural); // users table
         return response($columns);
     }
 }
