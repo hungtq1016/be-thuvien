0592388e12e7731e938162e0db183fe11683e2ff
hungtq1016 <hungtq1016@gmail.com>
2023-01-09 00:59:44 +0700

    modal

--- resources/components/admin/ModalComponent.vue
+++ resources/components/admin/ModalComponent.vue
@@ -1,62 +1,25 @@
 <template>
   <TransitionRoot appear :show="isClose" as="template">
     <Dialog as="div" class="relative z-10">
-      <TransitionChild
-        as="template"
-        enter="duration-300 ease-out"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="duration-200 ease-in"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100" leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0" >
         <div class="fixed inset-0 bg-black bg-opacity-25" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-y-auto">
-        <div
-          class="flex min-h-full items-center justify-center p-4 text-center"
-        >
-          <TransitionChild
-            as="template"
-            enter="duration-300 ease-out"
-            enter-from="opacity-0 scale-95"
-            enter-to="opacity-100 scale-100"
-            leave="duration-200 ease-in"
-            leave-from="opacity-100 scale-100"
-            leave-to="opacity-0 scale-95"
-          >
-            <DialogPanel
-              class="
-              w-full max-w-xl transform overflow-hidden rounded-2xl bg-white dark:bg-zinc-600 p-6 text-left align-middle shadow-xl transition-all
-              "
-            >
-              <DialogTitle
-                as="h3"
-                class="text-lg font-medium leading-6 text-slate-900 dark:text-white"
-              >
+        <div class="flex min-h-full items-center justify-center p-4 text-center" >
+          <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95" >
+            <DialogPanel class=" w-full max-w-xl transform overflow-hidden rounded-2xl bg-white dark:bg-zinc-600 p-6 text-left align-middle shadow-xl transition-all " >
+              <DialogTitle as="h3" class="text-lg font-medium leading-6 text-slate-900 dark:text-white" >
                 {{idProp == null? "Thêm" : "Chỉnh Sửa" }} {{ this.$route.meta.title }}
               </DialogTitle>
               <div class="mt-2">
                 <KeepAlive>
-                  <component
-                    :is="this.form"
-                    :idProp="this.idProp"
-                    :resource="resource"
-                    @closeModal="
-                      isClose = !false;
-                      $emit('closeModal');
-                    "
-                    @closeAndUpdate="$emit('update'); $emit('closeModal');isClose = !false;"
-                  />
+                  <component :is="this.form" :idProp="this.idProp" :resource="resource" @closeModal=" isClose = !false; $emit('closeModal'); "
+                   @closeAndUpdate="$emit('update'); $emit('closeModal');isClose = !false;" />
                 </KeepAlive>
               </div>
               <div class="absolute top-6 right-6">
-                <button
-                  type="button"
-                  class=""
-                  @click="(isClose = !isClose), $emit('closeModal')"
-                >
+                <button type="button" class="" @click="(isClose = !isClose), $emit('closeModal')" >
                   <XMarkIcon class="w-6 h-6 dark:text-white"/>
                 </button>
               </div>
@@ -72,31 +35,10 @@ import tagForm from "../../views/admin/tag/TagForm.vue";
 import categoryForm from "../../views/admin/category/CategoryForm.vue";
 import actorForm from "../../views/admin/actor/ActorForm.vue";
 import directorForm from "../../views/admin/director/DirectorForm.vue";
-
-
-import {
-    XMarkIcon,
-} from "@heroicons/vue/24/outline";
-import {
-  TransitionRoot,
-  TransitionChild,
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/vue";
+import { XMarkIcon, } from "@heroicons/vue/24/outline";
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle, } from "@headlessui/vue";
 export default {
-  components: {
-    tagForm,
-    categoryForm,
-    actorForm,
-    directorForm,
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogPanel,
-    DialogTitle,
-    XMarkIcon
-  },
+  components: { tagForm, categoryForm, actorForm, directorForm, TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle, XMarkIcon },
   props: ["openModal", "idProp","form","resource"],
   data() {
     return {
@@ -106,7 +48,6 @@ export default {
   },
   watch: {
     openModal: function (newVal, oldVal) {
-      // watch it
       this.isClose = newVal;
     },
   },
